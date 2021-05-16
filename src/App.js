2038b1dfb4e4a98d9d9d8cb88bc0b998e2ff44a7
chima2g <chima2g@hotmail.com>
2021-05-16 19:04:49 +0100

    create TaxCalculator component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
-
+import TaxCalculator from "./TaxCalculator";
 function App() {
-  return <div className="App">
-    
-  </div>;
+  return (
+    <div className="App">
+      <TaxCalculator></TaxCalculator>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/TaxCalculator.js
@@ -0,0 +1,5 @@
+const TaxCalculator = () => {
+  return <div></div>;
+};
+
+export default TaxCalculator;
